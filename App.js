85f88bc25ff76195de97035f80c0c861310be2c7
leonardo442 <112430425+leonardo442@users.noreply.github.com>
2024-02-19 17:15:12 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import {exibirHorario} from './components/Horario';
+import { fazerLogin } from './components/login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +20,15 @@ function App() {
         >
           Learning React
         </a>
+         <br />
+        <input type="text" id="txtLogin" />
+        <br />
+        <input type="text" id="txtSenha" />
+        <br />
+        <button
+          onClick={ fazerLogin}
+        >fazerLogin</button>
+        <br />
         <button
         onClick={exibirHorario}
         >ver Horario</button>
